bea1bbddbd3d482a33cc2dce7d7ab3a14ddad017
medina-jose <jose.medina2073@gmail.com>
2019-12-07 10:49:00 -0500

    added disconnect (node.js implementation of discogs api) to package.json and running test get

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const path = require('path');
-const port = 3000
+const port = 3000;
+var discogs = require('disconnect').Client;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// test discogs
+var db = new discogs().database();
+db.getRelease(176126, function(err, data) {
+    console.log(data);
+})
